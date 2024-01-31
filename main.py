da5dbd45ad198e84386f51956809ea5f1cd781ed
rachit-j <rjaiswal.sd.77@gmail.com>
2024-01-30 20:22:38 -0800

    cors?

--- main.py
+++ main.py
@@ -102,4 +102,6 @@ app.cli.add_command(custom_cli)
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
+    from flask_cors import CORS
+    cors = CORS(app)
     app.run(debug=True, host="0.0.0.0", port="8086")
